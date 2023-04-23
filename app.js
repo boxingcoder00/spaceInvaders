407fc2837904c4180567fb4a532f3877e2ccd6b4
boxingcoder00 <111091683+boxingcoder00@users.noreply.github.com>
2023-04-23 13:02:20 -0400

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ let results = 0
 for(let i = 0; i < 225; i++) {
     const square = document.createElement('div')
     grid.appendChild(square)
-}
+};
 
 const squares = array.from(document.querySelectorAll('.grid div'))
 
@@ -19,23 +19,23 @@ const alienInvaders = [
     0,1,2,3,4,5,6,7,8,9,
     15,16,17,18,19,20,21,22,23,24,
     30,31,32,33,34,35,36,37,38,39
-]
+];
 
 function draw() {
     for (let i = 0; i < alienInvaders.length; i++) {
         if(!aliensRemoved.includes(i)) {
         squares[alienInvaders[i]].classList.add('invader')
-        }
-    }
-}
+        };
+    };
+};
 
 draw()
 
 function remove() {
     for (let i = 0; i < alienInvaders.length; i++) {
         squares[alienInvaders[i]].classList.remove('invader')
-    }
-}
+    };
+};
 
 squares[currentShooterIndex].classList.add('shooter')
 
@@ -52,6 +52,7 @@ function moveShooter() {
         }    
         squares[currentShooterIndex].classList.add('shooter')
 }
+
 document.addEventListener('keydown', moveShooter)
 
 function moveInvaders() {
@@ -98,7 +99,7 @@ if (aliensRemoved.length === alienInvaders.length) {
     clearInterval(invadersID)
 }
 
-}
+    
 invadersId = setInterval(moveInvaders, 400)
 
 function shoot(e) {
